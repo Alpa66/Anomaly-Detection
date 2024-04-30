ff160d29678fd1ce08c92729f2668812633e6011
ahmadah <akhmad.a@phystech.edu>
2024-04-30 21:34:11 +0300

    Experiments with constant seed

--- layers.py
+++ layers.py
@@ -2,10 +2,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pandas as pd
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.metrics import confusion_matrix, f1_score, roc_curve, roc_auc_score, precision_score, recall_score
+from torch_geometric.data import Data
 from torch.utils.data import Dataset
 
 # Graph Convolutional Layer:
@@ -77,66 +79,6 @@ class Graph_Directed_A(nn.Module):
         return adj
 
 
-# A for Uni-directed graphs:
-class Graph_Uni_Directed_A(nn.Module):
-      
-    def __init__(self, num_nodes, window_size, alpha, k, device):
-        super(Graph_Directed_A, self).__init__()
-        
-        self.alpha = alpha
-        self.k = k
-        self.device = device
-        
-        self.e1 = nn.Embedding(num_nodes, window_size)
-        self.e2 = nn.Embedding(num_nodes, window_size)
-        self.l1 = nn.Linear(window_size,window_size)
-        self.l2 = nn.Linear(window_size,window_size)
-        
-    def forward(self, idx):
-        
-        m1 = torch.tanh(self.alpha*self.l1(self.e1(idx)))
-        m2 = torch.tanh(self.alpha*self.l2(self.e2(idx)))
-        adj = F.relu(torch.tanh(self.alpha*(torch.mm(m1, m2.transpose(1,0)) - torch.mm(m2, m1.transpose(1,0)))))
-        
-        if self.k:
-            mask = torch.zeros(idx.size(0), idx.size(0)).to(self.device)
-            mask.fill_(float('0'))
-            s1,t1 = (adj + torch.rand_like(adj)*0.01).topk(self.k,1)
-            mask.scatter_(1,t1,s1.fill_(1))
-            adj = adj*mask
-        
-        return adj
-                            
-            
-# A for Undirected graphs:
-class Graph_Undirected_A(nn.Module):
-      
-    def __init__(self, num_nodes, window_size, alpha, k, device):
-        super(Graph_Directed_A, self).__init__()
-        
-        self.alpha = alpha
-        self.k = k
-        self.device = device
-        
-        self.e1 = nn.Embedding(num_nodes, window_size)
-        self.l1 = nn.Linear(window_size,window_size)
-        
-    def forward(self, idx):
-        
-        m1 = torch.tanh(self.alpha*self.l1(self.e1(idx)))
-        m2 = torch.tanh(self.alpha*self.l1(self.e1(idx)))
-        adj = F.relu(torch.tanh(self.alpha*torch.mm(m1, m2.transpose(1,0))))
-        
-        if self.k:
-            mask = torch.zeros(idx.size(0), idx.size(0)).to(self.device)
-            mask.fill_(float('0'))
-            s1,t1 = (adj + torch.rand_like(adj)*0.01).topk(self.k,1)
-            mask.scatter_(1,t1,s1.fill_(1))
-            adj = adj*mask
-        
-        return adj
-
-
 class SWat_dataset(Dataset):
     def __init__(self, dataframe: pd.DataFrame, target: pd.DataFrame,  window_size, device):
         self.data = dataframe
@@ -172,7 +114,6 @@ class SWat_dataset_window_last(Dataset):
         self.data = dataframe
         self.window_size = window_size
         self.device = device
-        self.num_sensor = dataframe.shape[1]
 
     def __len__(self):
         return len(self.data) - self.window_size
@@ -181,3 +122,36 @@ class SWat_dataset_window_last(Dataset):
         window = self.data[idx: idx + self.window_size]
         features = torch.tensor(window.iloc[:,:].values).float().to(self.device)
         return torch.transpose(features, 0, 1)
+
+
+def get_edges(adj):
+    device = adj.device
+    row, col = adj.nonzero(as_tuple=True)
+    edge_index = torch.stack([row, col], dim=0).float().to(device)
+    return edge_index
+
+
+class SWat_dataset_GAT(Dataset):
+    def __init__(self, dataframe: pd.DataFrame, target: pd.DataFrame,  window_size, edge_index, device):
+        self.data = dataframe
+        self.window_size = window_size
+        self.device = device
+        self.edge_index = edge_index
+
+    def __len__(self):
+        return len(self.data) - self.window_size
+
+    def __getitem__(self, idx):
+        window = self.data[idx: idx + self.window_size]
+        features = torch.tensor(window.iloc[:,:].values).float().to(self.device)
+        features = torch.transpose(features, 0, 1)
+        return features
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
